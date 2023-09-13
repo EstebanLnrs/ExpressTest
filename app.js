d1bb04678a11f054f21fc05dc2348c82bb3a127e
EstebanLnrs <esteb-62@hotmail.fr>
2023-09-13 13:02:20 +0200

    sc

--- app.js
+++ app.js
@@ -27,6 +27,22 @@ connection.connect((err) => {
 
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  
+  app.use((req, res) => {
+    res.json({ message: "UPDATE !" }); 
+ });
+
+
+
 app.listen(port, () => {
   console.log(`Serveur en écoute sur le port ${port}`);
 });
@@ -60,5 +76,7 @@ app.get('/api/data', (req, res) => {
       }
     });
   });
+
+module.exports = app;
 
 
